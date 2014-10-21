b7fc0f966e9c87f77e8c0dc7ea7138f43d3464cb
Damon Oehlman <damon.oehlman@gmail.com>
2014-10-22 10:44:47 +1100

    Added simple mocha test suite to replicate #11

---
+++ test/connect.js
@@ -0,0 +1,44 @@
+var assert = require('assert');
+var uuid = require('uuid');
+var RTC = require('../');
+var connections = [];
+var roomId = uuid.v4();
+
+suite('connectivity');
+
+test('create connection:0', function() {
+  var conn = RTC({ room: roomId, constraints: null });
+
+  assert(conn);
+  assert(conn.id);
+  connections.push(conn);
+});
+
+test('create connection:1', function() {
+  var conn = RTC({ room: roomId, constraints: null });
+
+  assert(conn);
+  assert(conn.id);
+  connections.push(conn);
+});
+
+test('receive call:started events for both connections', function(done) {
+  var expected = 2;
+
+  function checkExpected() {
+    expected--;
+    if (expected <= 0) {
+      done();
+    }
+  }
+
+  connections[0].once('call:started', function(id) {
+    assert.equal(id, connections[1].id);
+    checkExpected();
+  });
+
+  connections[1].once('call:started', function(id) {
+    assert.equal(id, connections[0].id);
+    checkExpected();
+  });
+});
